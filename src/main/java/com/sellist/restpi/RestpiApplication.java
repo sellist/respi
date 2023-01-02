4cc381bfab2fafefa490b1b1330eed26b159c751
Scott Ellis <scottellis94@gmail.com>
2023-01-02 15:55:32 -0500

    initial commit

---
+++ src/main/java/com/sellist/restpi/RestpiApplication.java
@@ -0,0 +1,13 @@
+package com.sellist.restpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestpiApplication.class, args);
+	}
+
+}
