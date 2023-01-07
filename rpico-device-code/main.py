47f5477c8e4f38d0f60473aa7c83c4aea6313629
Scott Ellis <scottellis94@gmail.com>
2023-01-06 23:58:30 -0500

    working http requests

--- rpico-device-code/main.py
+++ rpico-device-code/main.py
@@ -6,10 +6,10 @@ import time
 import network
 import urequests
 import uasyncio
+import json
 
-_SSID = ""
-_PASS = ""
-_URL = ""
+SSID = "Celery-Man"
+PASS = "oyster-smiling"
 
 board_led = Pin("LED", Pin.OUT)
 
@@ -21,18 +21,19 @@ lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)
 #network setup
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect(_SSID, _PASS)
+wlan.connect(SSID, PASS)
 
-while not wlan.isconnected() and wlan.status() >= 0:
-    lcd.putstr("Waiting to\nconnect:")
-    time.sleep(1)
-    lcd.clear()
+timeout_counter = 0
+
+if not wlan.isconnected():
+    lcd.putstr("Connecting u")
+    while not wlan.isconnected() and wlan.status() >= 0:
+        print(f"connecting {timeout_counter}")
 
 async def main():
 
     switch_pin = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
     button_one = Switch(switch_pin)
-    print("here")
     lcd.clear()
     lcd.putstr("Pico LCD example")
     time.sleep(1)
@@ -55,15 +56,23 @@ async def main():
         # If my switch had a new value, print the new state
         if my_switch_new_value:
             if my_switch_value:
-                lcd.clear()
-                thing = await request_message()
-                lcd.putstr(thing)
-            else:
-                pass
-                #lcd.clear()
+                print_to_lcd(lcd, "asking server")
+                top = await request_message()
+                print_to_lcd(lcd, top)
+
+def print_to_lcd(lcd, str1="", str2=""):
+    lcd.clear()
+    lcd.putstr(str1)
+    lcd.putstr("\n")
+    lcd.putstr(str2)
 
 async def request_message():
-    return urequests.get(_URL)
+    try:
+        r = urequests.get("https://random-data-api.com/api/v2/appliances")
+        return r.json()['equipment']
+    except Exception as e:
+        print(e)
+        print_to_lcd(lcd, "error")
+        return "error"
 
-print('hellohellohello')
 uasyncio.run(main())
